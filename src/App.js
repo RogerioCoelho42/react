be21d1fb8e96923959cfb53322aec9ee12332d22
Rogerio <rogerio@tidea.com.br>
2020-04-28 21:33:00 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    listRepositories();
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post("repositories", {
+      title: `Titulo ${Date.now()}`,
+      url: "http://www.teste.com.br",
+      techs: ["React", "React Native"],
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    const repo = [...repositories];
+    const index = repo.findIndex((repository) => repository.id === id);
+    repo.splice(index, 1);
+    setRepositories(repo);
+  }
+
+  async function listRepositories() {
+    const response = await api.get("repositories");
+    setRepositories(response.data);
+  }
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
